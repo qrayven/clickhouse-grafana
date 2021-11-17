fce79b028125ae8391e8c0a5c9a3f2b22c0e6944
qrayven <rayven@null.net>
2021-11-17 10:47:48 +0100

    prepare to new release, current_version = 2.3.1

--- release.sh
+++ release.sh
@@ -5,7 +5,8 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 if [[ $OSTYPE == *linux* ]]; then
-    echo 1 > /proc/sys/vm/drop_caches
+  echo "something"
+    #echo 1 > /proc/sys/vm/drop_caches
 fi
 source .release_env
 git config core.eol lf
